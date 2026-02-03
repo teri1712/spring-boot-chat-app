aadb40ddc0bc0f57b2380aa168e7aa56cac765f3
decade <20120220@student.hcmus.edu.vn>
2026-02-03 16:59:37 +0700

    fix: Docker bug caused by refactoring folders

--- source code/practice/src/main/java/com/decade/practice/infra/schedule/SchedulingConfiguration.java
+++ source code/practice/src/main/java/com/decade/practice/infra/schedule/SchedulingConfiguration.java
@@ -2,9 +2,10 @@ package com.decade.practice.infra.schedule;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-//@EnableScheduling
+@EnableScheduling
 @ConditionalOnProperty(name = "outbox.scheduling.enabled", havingValue = "true", matchIfMissing = true)
 public class SchedulingConfiguration {
 }
