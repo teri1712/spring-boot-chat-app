187b7c1603247e0ae33299b050401e8afc776658
decade <20120220@student.hcmus.edu.vn>
2026-04-29 18:05:05 +0700

    fix: resolve comments

--- source code/practice/src/main/java/com/decade/practice/search/adapter/SearchController.java
+++ source code/practice/src/main/java/com/decade/practice/search/adapter/SearchController.java
@@ -3,7 +3,14 @@ package com.decade.practice.search.adapter;
 import com.decade.practice.search.application.queries.SearchService;
 import com.decade.practice.search.dto.MessageResponse;
 import com.decade.practice.search.dto.PeopleResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ProblemDetail;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +22,16 @@ import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
+@SecurityRequirement(name = "bearerAuth")
 public class SearchController {
 
     private final SearchService searchService;
 
     @GetMapping("/people")
-    // TODO: Fix client endpoint
+    @Operation(summary = "Search people by name, keywords",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "List of matched users")
+        })
     public List<PeopleResponse> findUsers(
         @RequestParam String query
     ) {
@@ -28,6 +39,17 @@ public class SearchController {
     }
 
     @GetMapping("/chat-histories/{chatId}")
+    @Operation(
+        summary = "Find messages in a chat",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "List of matched messages"),
+            @ApiResponse(responseCode = "404", description = "Chat not found", content = @Content(
+                mediaType = "application/problem+json",
+                schema = @Schema(implementation = ProblemDetail.class),
+                examples = {@ExampleObject(ref = "#/components/examples/NotFound")}
+            ))
+        }
+    )
     public List<MessageResponse> findMessages(
         @AuthenticationPrincipal(expression = "id") UUID userId,
         @PathVariable String chatId,
